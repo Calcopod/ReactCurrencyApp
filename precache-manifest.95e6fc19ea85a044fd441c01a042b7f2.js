d158da0bc0a5958d2a0f3cfe87b799de8f1a3451
CalcoBoii <rosca.serban2@gmail.com>
2020-07-15 12:56:54 +0300

    Updates

---
+++ precache-manifest.95e6fc19ea85a044fd441c01a042b7f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d210aa67273bfc1d1f52007d589b5f07",
+    "url": "/ReactCurrencyApp/index.html"
+  },
+  {
+    "revision": "30674951f69c47367cc5",
+    "url": "/ReactCurrencyApp/static/css/main.1622dc93.chunk.css"
+  },
+  {
+    "revision": "c733cf7963be1c15d7dd",
+    "url": "/ReactCurrencyApp/static/js/2.6b66c650.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactCurrencyApp/static/js/2.6b66c650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30674951f69c47367cc5",
+    "url": "/ReactCurrencyApp/static/js/main.a1717644.chunk.js"
+  },
+  {
+    "revision": "73615486a132eb1e4963",
+    "url": "/ReactCurrencyApp/static/js/runtime-main.f62745d9.js"
+  }
+]);
